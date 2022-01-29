9b37eae8d9f144e1ab897867d0960951edeaf83c
@msLalith <santosh2397@gmail.com>
2022-01-29 10:21:16 +0530

    migrate to Version Catalog (buildSrc)

---
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -0,0 +1,59 @@
+object Libs {
+    const val buildToolsGradle = "com.android.tools.build:gradle:${Versions.GRADLE}"
+    const val buildToolsKotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.KOTLIN}"
+
+    const val buildToolsGoogleServices = "com.google.gms:google-services:${Versions.GOOGLE_SERVICES}"
+    const val buildToolsCrashlyticsGradle = "com.google.firebase:firebase-crashlytics-gradle:${Versions.CRASHLYTICS_GRADLE}"
+    const val buildToolsHiltAndroidGradlePlugin = "com.google.dagger:hilt-android-gradle-plugin:${Versions.HILT}"
+    const val buildToolsProtobufGradlePlugin = "com.google.protobuf:protobuf-gradle-plugin:${Versions.PROTOBUF_GRADLE}"
+
+    const val coreKtx = "androidx.core:core-ktx:${Versions.KOTLIN_CORE_KTX}"
+    const val kotlinxDateTime = "org.jetbrains.kotlinx:kotlinx-datetime:${Versions.KOTLINX_DATETIME}"
+
+    const val googleMaterial = "com.google.android.material:material:${Versions.GOOGLE_MATERIAL}"
+    const val googleProtobufJavalite = "com.google.protobuf:protobuf-javalite:${Versions.GOOGLE_PROTOBUF_JAVALITE}"
+    const val googlePlayCoreKtx = "com.google.android.play:core-ktx:${Versions.GOOGLE_PLAY_CORE_KTX}"
+    const val paletteKtx = "androidx.palette:palette-ktx:${Versions.PALETTE_KTX}"
+
+    const val composeMaterial = "androidx.compose.material:material:${Versions.COMPOSE}"
+    const val composeUi = "androidx.compose.ui:ui:${Versions.COMPOSE}"
+    const val composeUiTooling = "androidx.compose.ui:ui-tooling:${Versions.COMPOSE}"
+    const val lifecycleRuntimeKtx = "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.LIFECYCLE_RUNTIME_KTX}"
+    const val activityCompose = "androidx.activity:activity-compose:${Versions.ACTIVITY_COMPOSE}"
+    const val constraintLayoutCompose = "androidx.constraintlayout:constraintlayout-compose:${Versions.CONSTRAINT_LAYOUT_COMPOSE}"
+    const val navigationCompose = "androidx.navigation:navigation-compose:${Versions.NAVIGATION_COMPOSE}"
+    const val lifecycleViewModelCompose = "androidx.lifecycle:lifecycle-viewmodel-compose:${Versions.LIFECYCLE_VIEWMODEL_COMPOSE}"
+
+    const val dataStore = "androidx.datastore:datastore:${Versions.DATASTORE}"
+    const val dataStorePreferences = "androidx.datastore:datastore-preferences:${Versions.DATASTORE}"
+
+    const val firebaseBom = "com.google.firebase:firebase-bom:${Versions.FIREBASE_BOM}"
+    const val firebaseCrashlyticsKtx = "com.google.firebase:firebase-crashlytics-ktx"
+    const val firebaseAnalyticsKtx = "com.google.firebase:firebase-analytics-ktx"
+
+    const val hiltAndroid = "com.google.dagger:hilt-android:${Versions.HILT}"
+    const val hiltAndroidCompiler = "com.google.dagger:hilt-android-compiler:${Versions.HILT}"
+
+    const val hiltLifecycleViewModel = "androidx.hilt:hilt-lifecycle-viewmodel:${Versions.HILT_VIEWMODEL}"
+    const val hiltCompiler = "androidx.hilt:hilt-compiler:${Versions.HILT_VIEWMODEL}"
+    const val hiltNavigationCompose = "androidx.hilt:hilt-navigation-compose:${Versions.HILT_NAVIGATION_COMPOSE}"
+
+    const val roomKtx = "androidx.room:room-ktx:${Versions.ROOM}"
+    const val roomRuntime = "androidx.room:room-runtime:${Versions.ROOM}"
+    const val roomCompiler = "androidx.room:room-compiler:${Versions.ROOM}"
+
+    const val accompanistPager = "com.google.accompanist:accompanist-pager:${Versions.ACCOMPANIST}"
+    const val accompanistSystemUiController = "com.google.accompanist:accompanist-systemuicontroller:${Versions.ACCOMPANIST}"
+    const val accompanistInsets = "com.google.accompanist:accompanist-insets:${Versions.ACCOMPANIST}"
+    const val accompanistFlowLayout = "com.google.accompanist:accompanist-flowlayout:${Versions.ACCOMPANIST}"
+
+    const val retrofit = "com.squareup.retrofit2:retrofit:${Versions.RETROFIT}"
+    const val retrofitGsonConverter = "com.squareup.retrofit2:converter-gson:${Versions.RETROFIT}"
+
+    const val thirdSunCalc = "org.shredzone.commons:commons-suncalc:${Versions.THIRD_SUNCALC}"
+
+    const val testJUnit = "junit:junit:${Versions.TEST_JUNIT}"
+    const val testAndroidXJUnit = "androidx.test.ext:junit:${Versions.TEST_ANDROIDX_JUNIT}"
+    const val testAndroidXEspresso = "androidx.test.espresso:espresso-core:${Versions.TEST_ANDROIDX_ESPRESSO}"
+    const val testComposeJUnit = "androidx.compose.ui:ui-test-junit4:${Versions.COMPOSE}"
+}
---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,43 @@
+object Versions {
+    const val KOTLIN = "1.6.10"
+    const val GRADLE = "7.2.0-beta01"
+
+    const val GOOGLE_SERVICES = "4.3.10"
+    const val CRASHLYTICS_GRADLE = "2.8.1"
+    const val PROTOBUF_GRADLE = "0.8.17"
+
+    const val KOTLIN_CORE_KTX = "1.8.0-alpha03"
+    const val KOTLINX_DATETIME = "0.2.1"
+
+    const val GOOGLE_MATERIAL = "1.5.0"
+    const val GOOGLE_PROTOBUF_JAVALITE = "3.15.6"
+    const val GOOGLE_PLAY_CORE_KTX = "1.8.1"
+    const val PALETTE_KTX = "1.0.0"
+
+    const val COMPOSE = "1.2.0-alpha02"
+    const val LIFECYCLE_RUNTIME_KTX = "2.4.0"
+    const val ACTIVITY_COMPOSE = "1.4.0"
+    const val CONSTRAINT_LAYOUT_COMPOSE = "1.0.0"
+    const val NAVIGATION_COMPOSE = "2.5.0-alpha01"
+    const val LIFECYCLE_VIEWMODEL_COMPOSE = "2.4.0"
+
+    const val DATASTORE = "1.0.0"
+
+    const val FIREBASE_BOM = "28.2.1"
+
+    const val HILT = "2.39.1"
+    const val HILT_VIEWMODEL = "1.0.0-alpha03"
+    const val HILT_NAVIGATION_COMPOSE = "1.0.0-rc01"
+
+    const val ROOM = "2.4.1"
+
+    const val ACCOMPANIST = "0.24.1-alpha"
+
+    const val RETROFIT = "2.9.0"
+
+    const val THIRD_SUNCALC = "3.5"
+
+    const val TEST_JUNIT = "4.13.2"
+    const val TEST_ANDROIDX_JUNIT = "1.1.3"
+    const val TEST_ANDROIDX_ESPRESSO = "3.4.0"
+}
