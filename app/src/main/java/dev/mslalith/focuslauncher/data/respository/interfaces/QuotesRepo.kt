3a8a79d4794d65ad63981bb2a033b1ba0d198d9e
mslalith <santosh2397@gmail.com>
2022-04-29 19:53:00 +0530

    refactor: rename flow to stateflow

--- app/src/main/java/dev/mslalith/focuslauncher/data/respository/interfaces/QuotesRepo.kt
+++ app/src/main/java/dev/mslalith/focuslauncher/data/respository/interfaces/QuotesRepo.kt
@@ -5,8 +5,8 @@ import dev.mslalith.focuslauncher.data.models.Outcome
 import kotlinx.coroutines.flow.StateFlow
 
 interface QuotesRepo {
-    val currentQuoteStateFlow: StateFlow<Outcome<Quote>>
-    val isFetchingQuotesStateFlow: StateFlow<Boolean>
+    val currentQuoteStateStateFlow: StateFlow<Outcome<Quote>>
+    val isFetchingQuotesStateStateFlow: StateFlow<Boolean>
 
     suspend fun nextRandomQuote()
     suspend fun fetchQuotes(maxPages: Int = 2)
