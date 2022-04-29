0c4d2e18b5cdb818dde1b230c6998332399a2112
mslalith <santosh2397@gmail.com>
2022-04-29 19:07:22 +0530

    refactor: add QuotesRepo interface to create FakeQuotesRepo

---
+++ app/src/androidTest/java/dev/mslalith/focuslauncher/FakeQuotesApi.kt
@@ -0,0 +1,48 @@
+package dev.mslalith.focuslauncher
+
+import dev.mslalith.focuslauncher.data.api.QuotesApi
+import dev.mslalith.focuslauncher.data.database.entities.Quote
+import dev.mslalith.focuslauncher.data.models.QuoteResponse
+import dev.mslalith.focuslauncher.data.models.QuotesApiResponse
+
+class FakeQuotesApi : QuotesApi {
+    override suspend fun getQuotes(page: Int, limit: Int): QuotesApiResponse {
+        val quotes = TestQuotes.buildQuotes(page, limit).map(TestQuotes::toQuoteResponse)
+        return QuotesApiResponse(
+            count = quotes.size,
+            totalCount = quotes.size,
+            page = page,
+            totalPages = 5,
+            lastItemIndex = 5,
+            results = quotes
+        )
+    }
+}
+
+private object TestQuotes {
+
+    fun buildQuotes(page: Int, limit: Int) = buildList {
+        val start = (page - 1) * limit
+        repeat(limit) {
+            add(buildSingleQuote(start + it))
+        }
+    }
+
+    fun buildSingleQuote(index: Int) = Quote(
+        id = (1000 * index).toString(),
+        tags = listOf("tag_1", "tag_2"),
+        quote = "Quote #$index",
+        author = "Author #$index",
+        authorSlug = "author_$index",
+        length = index
+    )
+
+    fun toQuoteResponse(quote: Quote) = QuoteResponse(
+        id = quote.id,
+        quote = quote.quote,
+        author = quote.author,
+        authorSlug = quote.authorSlug,
+        length = quote.length,
+        tags = quote.tags
+    )
+}
