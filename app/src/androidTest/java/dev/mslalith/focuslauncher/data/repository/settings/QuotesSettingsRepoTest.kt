3171dd4256dad97fcdf43fd1b992f765353c9482
mslalith <mslalith.opensource@gmail.com>
2022-05-03 08:46:34 +0530

    chore: fix ktlint issues

--- app/src/androidTest/java/dev/mslalith/focuslauncher/data/repository/settings/QuotesSettingsRepoTest.kt
+++ app/src/androidTest/java/dev/mslalith/focuslauncher/data/repository/settings/QuotesSettingsRepoTest.kt
@@ -1,11 +1,11 @@
 package dev.mslalith.focuslauncher.data.repository.settings
 
-import dev.mslalith.focuslauncher.data.repository.DataStoreTest
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.runTest
 import com.google.common.truth.Truth.assertThat
+import dev.mslalith.focuslauncher.data.repository.DataStoreTest
 import dev.mslalith.focuslauncher.utils.Constants
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
 import org.junit.Test
 
 @OptIn(ExperimentalCoroutinesApi::class)
