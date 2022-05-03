24b3722ceda0829f1f3b95637a6bd3072f18d0b1
mslalith <mslalith.opensource@gmail.com>
2022-05-03 12:43:12 +0530

    refactor: change dependencies in Libs & data module

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,15 +1,17 @@
 plugins {
     id("com.android.library")
-    id("org.jetbrains.kotlin.android")
+    kotlin("android")
+    kotlin("kapt")
+    // id("dagger.hilt.android.plugin")
 }
 
 android {
     namespace = "dev.mslalith.focuslauncher.data"
-    compileSdk = 32
+    compileSdk = ConfigData.TARGET_SDK
 
     defaultConfig {
-        minSdk = 21
-        targetSdk = 32
+        minSdk = ConfigData.MIN_SDK
+        targetSdk = ConfigData.TARGET_SDK
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
@@ -31,11 +33,8 @@ android {
 }
 
 dependencies {
+    androidxCoreKtx()
+    room()
 
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.4.1")
-    implementation("com.google.android.material:material:1.5.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    junit()
 }
