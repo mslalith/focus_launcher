a8c752882249f44627a5dca23d51bb1b8d27210a
mslalith <mslalith.opensource@gmail.com>
2022-05-03 11:51:46 +0530

    refactor: add data module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,8 @@ android {
 }
 
 dependencies {
+    implementation(project(":data"))
+
     kotlin()
     google()
     androidx()
---
+++ data/src/main/java/dev/mslalith/focuslauncher/data/App.kt
@@ -0,0 +1,7 @@
+package dev.mslalith.focuslauncher.data
+
+data class App(
+    val name: String,
+    val packageName: String,
+    val isSystem: Boolean,
+)
