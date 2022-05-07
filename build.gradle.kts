15b839348cba1c8193c7587bbe520bafa29558e9
@msLalith <mslalith.opensource@gmail.com>
2022-05-07 14:01:53 +0530

    Move Git Hooks configuration to separate script file (#12)
    * chore: move ktlint pre-push hook to it's own build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,5 @@
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 import com.github.benmanes.gradle.versions.updates.gradle.GradleReleaseChannel
-import java.nio.file.Files
-import java.nio.file.Paths
-import java.nio.file.attribute.PosixFilePermission
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
@@ -18,6 +15,7 @@ buildscript {
     }
 }
 
+apply(from = "./buildScripts/install-git-hooks.gradle.kts")
 apply(plugin = "kover")
 
 // ktlint-gradle
@@ -64,21 +62,3 @@ tasks.named<DependencyUpdatesTask>("dependencyUpdates").configure {
     outputDir = "build/dependencyUpdates"
     reportfileName = "dependency_update_report"
 }
-
-// Create a task to copy Git hooks from /scripts to .git/hooks path
-val installGitHooks by tasks.creating(Copy::class) {
-    from(layout.projectDirectory.file("scripts/pre-push"))
-    val toDir = layout.projectDirectory.dir(".git/hooks")
-    val toFile = toDir.file("pre-push").asFile
-    val toFilePath = Paths.get(toFile.absolutePath)
-    into(toDir)
-
-    doLast {
-        val perms = Files.getPosixFilePermissions(toFilePath)
-        perms.add(PosixFilePermission.OWNER_EXECUTE)
-        Files.setPosixFilePermissions(toFilePath, perms)
-    }
-}
-
-// Register the Git task to run at beginning
-tasks.getByPath(":app:preBuild").dependsOn(installGitHooks)
