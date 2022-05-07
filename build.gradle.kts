3d09cf4410b24ea07eeb46d0089d18ab8dab07f0
mslalith <mslalith.opensource@gmail.com>
2022-05-07 13:48:26 +0530

    # Conflicts: #	build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -62,3 +62,21 @@ tasks.named<DependencyUpdatesTask>("dependencyUpdates").configure {
     outputDir = "build/dependencyUpdates"
     reportfileName = "dependency_update_report"
 }
+
+// Create a task to copy Git hooks from /scripts to .git/hooks path
+val installGitHooks by tasks.creating(Copy::class) {
+    from(layout.projectDirectory.file("scripts/pre-push"))
+    val toDir = layout.projectDirectory.dir(".git/hooks")
+    val toFile = toDir.file("pre-push").asFile
+    val toFilePath = Paths.get(toFile.absolutePath)
+    into(toDir)
+
+    doLast {
+        val perms = Files.getPosixFilePermissions(toFilePath)
+        perms.add(PosixFilePermission.OWNER_EXECUTE)
+        Files.setPosixFilePermissions(toFilePath, perms)
+    }
+}
+
+// Register the Git task to run at beginning
+tasks.getByPath(":app:preBuild").dependsOn(installGitHooks)
