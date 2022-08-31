6a66a65df8c19e0ae6cda4b47ee67b35a133076b
mslalith <mslalith.opensource@gmail.com>
2022-08-31 20:08:58 +0530

    Bump version to 0.2.1

--- buildSrc/src/main/kotlin/ConfigData.kt
+++ buildSrc/src/main/kotlin/ConfigData.kt
@@ -2,6 +2,6 @@ object ConfigData {
     const val MIN_SDK = 26
     const val TARGET_SDK = 32
 
-    const val VERSION_CODE = 4
-    const val VERSION_NAME = "0.1.1"
+    const val VERSION_CODE = 5
+    const val VERSION_NAME = "0.2.1"
 }
