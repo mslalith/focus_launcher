c3479217edfd7e81fffcb3f768a1a51272c4ba6e
mslalith <mslalith.opensource@gmail.com>
2023-02-18 07:42:11 +0530

    build: update app version to 6 (v0.3.0)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
 
     defaultConfig {
         applicationId = "dev.mslalith.focuslauncher"
-        versionCode = 5
-        versionName = "0.2.1"
+        versionCode = 6
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
