708de214658d91ae35ae43c7319c935e8352e95e
mslalith <mslalith.opensource@gmail.com>
2023-02-24 13:40:46 +0530

    test: add AppDrawerRepoImplTest

---
+++ core/data/src/test/kotlin/dev/mslalith/focuslauncher/core/data/repository/impl/AppDrawerRepoImplTest.kt
@@ -0,0 +1,121 @@
+package dev.mslalith.focuslauncher.core.data.repository.impl
+
+import com.google.common.truth.Truth.assertThat
+import dev.mslalith.focuslauncher.core.data.base.RepoTest
+import dev.mslalith.focuslauncher.core.data.model.TestComponents
+import dev.mslalith.focuslauncher.core.testing.TestApps
+import dev.mslalith.focuslauncher.core.testing.extensions.awaitItem
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@OptIn(ExperimentalCoroutinesApi::class)
+@RunWith(RobolectricTestRunner::class)
+internal class AppDrawerRepoImplTest : RepoTest<AppDrawerRepoImpl>() {
+
+    override fun provideRepo(testComponents: TestComponents) = AppDrawerRepoImpl(
+        appsDao = testComponents.database.appsDao(),
+        appToRoomMapper = testComponents.mappers.appToRoomMapper
+    )
+
+    @Test
+    fun `initially app drawer list must be empty`() = runCoroutineTest {
+        val items = repo.allAppsFlow.awaitItem()
+        assertThat(items).isEmpty()
+    }
+
+    @Test
+    fun `when an app is added to app drawer, make sure it stays added`() = runCoroutineTest {
+        val app = TestApps.Chrome
+        repo.addApp(app)
+
+        val items = repo.allAppsFlow.awaitItem()
+        assertThat(items).isEqualTo(listOf(app))
+    }
+
+    @Test
+    fun `when multiple apps are added to app drawer, make sure they stays added`() = runCoroutineTest {
+        val apps = listOf(TestApps.Chrome, TestApps.Phone)
+        repo.addApps(apps)
+
+        val items = repo.allAppsFlow.awaitItem()
+        assertThat(items).isEqualTo(apps)
+    }
+
+    @Test
+    fun `when an app is removed from app drawer, make sure it isn't present`() = runCoroutineTest {
+        val app = TestApps.Chrome
+        repo.addApp(app)
+
+        var items = repo.allAppsFlow.awaitItem()
+        assertThat(items).isEqualTo(listOf(app))
+
+        repo.removeApp(app)
+
+        items = repo.allAppsFlow.awaitItem()
+        assertThat(items).doesNotContain(app)
+        assertThat(items).isEmpty()
+    }
+
+    @Test
+    fun `when apps are present in app drawer, list should not be empty`() = runCoroutineTest {
+        repo.addApps(TestApps.all)
+        assertThat(repo.areAppsEmptyInDatabase()).isFalse()
+    }
+
+    @Test
+    fun `when apps are not present in app drawer, list should be empty`() = runCoroutineTest {
+        assertThat(repo.allAppsFlow.awaitItem()).isEmpty()
+        assertThat(repo.areAppsEmptyInDatabase()).isTrue()
+    }
+
+    @Test
+    fun `when fetching for an existing app drawer app, getAppBy must return that app`() = runCoroutineTest {
+        val app = TestApps.Youtube
+        repo.addApp(app)
+
+        val items = repo.allAppsFlow.awaitItem()
+        assertThat(items).isEqualTo(listOf(app))
+
+        val fetchedApp = repo.getAppBy(app.packageName)
+        assertThat(fetchedApp).isEqualTo(app)
+    }
+
+    @Test
+    fun `when fetching for a non-existing app drawer app, getAppBy must return null`() = runCoroutineTest {
+        val app = TestApps.Youtube
+        repo.addApp(app)
+
+        val items = repo.allAppsFlow.awaitItem()
+        assertThat(items).isEqualTo(listOf(app))
+
+        val fetchedApp = repo.getAppBy("some.package")
+        assertThat(fetchedApp).isNull()
+    }
+
+    @Test
+    fun `when display name for an app drawer app is changed, new name must be returned`() = runCoroutineTest {
+        val app = TestApps.Youtube
+        repo.addApp(app)
+
+        val items = repo.allAppsFlow.awaitItem()
+        assertThat(items).isEqualTo(listOf(app))
+
+        val displayName = "You Tube"
+        repo.updateDisplayName(app, displayName)
+
+        val fetchedApp = repo.getAppBy(app.packageName)
+        assertThat(fetchedApp).isNotNull()
+        assertThat(fetchedApp?.displayName).isEqualTo(displayName)
+    }
+
+    @Test
+    fun `when display name for an app drawer app is not set, app name must be used`() = runCoroutineTest {
+        val app = TestApps.Youtube
+        repo.addApp(app)
+
+        val items = repo.allAppsFlow.awaitItem()
+        assertThat(items.map { it.displayName }).isEqualTo(listOf(app.displayName))
+    }
+}
