2f644b56c08431ff20d23a23e2516bacf9c9e7c5
mslalith <mslalith.opensource@gmail.com>
2023-02-26 08:28:23 +0530

    test: use hilt for AppDrawerRepoImplTest

--- core/data/src/test/kotlin/dev/mslalith/focuslauncher/core/data/repository/impl/AppDrawerRepoImplTest.kt
+++ core/data/src/test/kotlin/dev/mslalith/focuslauncher/core/data/repository/impl/AppDrawerRepoImplTest.kt
@@ -1,23 +1,37 @@
 package dev.mslalith.focuslauncher.core.data.repository.impl
 
 import com.google.common.truth.Truth.assertThat
-import dev.mslalith.focuslauncher.core.data.base.RepoTest
-import dev.mslalith.focuslauncher.core.data.model.TestComponents
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.HiltTestApplication
+import dev.mslalith.focuslauncher.core.testing.CoroutineTest
 import dev.mslalith.focuslauncher.core.testing.TestApps
 import dev.mslalith.focuslauncher.core.testing.extensions.awaitItem
+import javax.inject.Inject
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 @OptIn(ExperimentalCoroutinesApi::class)
+@HiltAndroidTest
 @RunWith(RobolectricTestRunner::class)
-internal class AppDrawerRepoImplTest : RepoTest<AppDrawerRepoImpl>() {
+@Config(application = HiltTestApplication::class)
+internal class AppDrawerRepoImplTest : CoroutineTest() {
 
-    override fun provideRepo(testComponents: TestComponents) = AppDrawerRepoImpl(
-        appsDao = testComponents.database.appsDao(),
-        appToRoomMapper = testComponents.mappers.appToRoomMapper
-    )
+    @get:Rule(order = 0)
+    var hiltRule = HiltAndroidRule(this)
+
+    @Inject
+    lateinit var repo: AppDrawerRepoImpl
+
+    @Before
+    fun setup() {
+        hiltRule.inject()
+    }
 
     @Test
     fun `initially app drawer list must be empty`() = runCoroutineTest {
