ca87b20ae82fdc5881078f5de4564ce75c4dc183
mslalith <mslalith.opensource@gmail.com>
2023-02-26 10:14:43 +0530

    test: use hilt for QuotesSettingsRepoTest

--- core/data/src/test/kotlin/dev/mslalith/focuslauncher/core/data/repository/impl/settings/QuotesSettingsRepoTest.kt
+++ core/data/src/test/kotlin/dev/mslalith/focuslauncher/core/data/repository/impl/settings/QuotesSettingsRepoTest.kt
@@ -1,19 +1,37 @@
 package dev.mslalith.focuslauncher.core.data.repository.impl.settings
 
-import dev.mslalith.focuslauncher.core.data.base.RepoTest
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.HiltTestApplication
 import dev.mslalith.focuslauncher.core.data.helpers.verifyChange
-import dev.mslalith.focuslauncher.core.data.model.TestComponents
+import dev.mslalith.focuslauncher.core.data.repository.settings.QuotesSettingsRepo
 import dev.mslalith.focuslauncher.core.data.utils.Constants.Defaults.Settings.Quotes.DEFAULT_SHOW_QUOTES
+import dev.mslalith.focuslauncher.core.testing.CoroutineTest
+import javax.inject.Inject
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 @OptIn(ExperimentalCoroutinesApi::class)
+@HiltAndroidTest
 @RunWith(RobolectricTestRunner::class)
-internal class QuotesSettingsRepoImplTest : RepoTest<QuotesSettingsRepoImpl>() {
+@Config(application = HiltTestApplication::class)
+internal class QuotesSettingsRepoTest : CoroutineTest() {
 
-    override fun provideRepo(testComponents: TestComponents) = QuotesSettingsRepoImpl(settingsDataStore = testComponents.dataStore)
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    @Inject
+    lateinit var repo: QuotesSettingsRepo
+
+    @Before
+    fun setup() {
+        hiltRule.inject()
+    }
 
     @Test
     fun `verify show quotes change`() = runCoroutineTest {
