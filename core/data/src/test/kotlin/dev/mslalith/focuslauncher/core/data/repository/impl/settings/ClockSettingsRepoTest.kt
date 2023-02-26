de9a5fba81ad642a2fc0d1390f6044516cdda1b4
mslalith <mslalith.opensource@gmail.com>
2023-02-26 10:11:50 +0530

    test: use hilt for ClockSettingsRepoTest

--- core/data/src/test/kotlin/dev/mslalith/focuslauncher/core/data/repository/impl/settings/ClockSettingsRepoTest.kt
+++ core/data/src/test/kotlin/dev/mslalith/focuslauncher/core/data/repository/impl/settings/ClockSettingsRepoTest.kt
@@ -1,22 +1,40 @@
 package dev.mslalith.focuslauncher.core.data.repository.impl.settings
 
-import dev.mslalith.focuslauncher.core.data.base.RepoTest
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.HiltTestApplication
 import dev.mslalith.focuslauncher.core.data.helpers.verifyChange
-import dev.mslalith.focuslauncher.core.data.model.TestComponents
+import dev.mslalith.focuslauncher.core.data.repository.settings.ClockSettingsRepo
 import dev.mslalith.focuslauncher.core.data.utils.Constants.Defaults.Settings.Clock.DEFAULT_CLOCK_24_ANIMATION_DURATION
 import dev.mslalith.focuslauncher.core.data.utils.Constants.Defaults.Settings.Clock.DEFAULT_CLOCK_ALIGNMENT
 import dev.mslalith.focuslauncher.core.data.utils.Constants.Defaults.Settings.Clock.DEFAULT_SHOW_CLOCK_24
 import dev.mslalith.focuslauncher.core.model.ClockAlignment
+import dev.mslalith.focuslauncher.core.testing.CoroutineTest
+import javax.inject.Inject
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 @OptIn(ExperimentalCoroutinesApi::class)
+@HiltAndroidTest
 @RunWith(RobolectricTestRunner::class)
-internal class ClockSettingsRepoImplTest : RepoTest<ClockSettingsRepoImpl>() {
+@Config(application = HiltTestApplication::class)
+internal class ClockSettingsRepoTest : CoroutineTest() {
 
-    override fun provideRepo(testComponents: TestComponents) = ClockSettingsRepoImpl(settingsDataStore = testComponents.dataStore)
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    @Inject
+    lateinit var repo: ClockSettingsRepo
+
+    @Before
+    fun setup() {
+        hiltRule.inject()
+    }
 
     @Test
     fun `verify show clock24 change`() = runCoroutineTest {
