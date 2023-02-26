64cfa67dd187f6aa53e11d0a5029adb30a524669
mslalith <mslalith.opensource@gmail.com>
2023-02-26 09:58:40 +0530

    test: use hilt for ThemeRepoTest

--- core/data/src/test/kotlin/dev/mslalith/focuslauncher/core/data/repository/impl/ThemeRepoTest.kt
+++ core/data/src/test/kotlin/dev/mslalith/focuslauncher/core/data/repository/impl/ThemeRepoTest.kt
@@ -1,19 +1,37 @@
 package dev.mslalith.focuslauncher.core.data.repository.impl
 
-import dev.mslalith.focuslauncher.core.data.base.RepoTest
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.HiltTestApplication
 import dev.mslalith.focuslauncher.core.data.helpers.verifyChange
-import dev.mslalith.focuslauncher.core.data.model.TestComponents
+import dev.mslalith.focuslauncher.core.data.repository.ThemeRepo
 import dev.mslalith.focuslauncher.core.model.Theme
+import dev.mslalith.focuslauncher.core.testing.CoroutineTest
+import javax.inject.Inject
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 @OptIn(ExperimentalCoroutinesApi::class)
+@HiltAndroidTest
 @RunWith(RobolectricTestRunner::class)
-internal class ThemeRepoImplTest : RepoTest<ThemeRepoImpl>() {
+@Config(application = HiltTestApplication::class)
+internal class ThemeRepoTest : CoroutineTest() {
 
-    override fun provideRepo(testComponents: TestComponents) = ThemeRepoImpl(themeDataStore = testComponents.dataStore)
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    @Inject
+    lateinit var repo: ThemeRepo
+
+    @Before
+    fun setup() {
+        hiltRule.inject()
+    }
 
     @Test
     fun `verify theme change`() = runCoroutineTest {
