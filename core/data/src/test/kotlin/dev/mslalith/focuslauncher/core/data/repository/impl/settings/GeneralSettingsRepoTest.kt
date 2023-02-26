15b56e51e84067c2a6dcc8d139fa1825b30e9fe3
mslalith <mslalith.opensource@gmail.com>
2023-02-26 10:12:44 +0530

    test: use hilt for GeneralSettingsRepoTest

--- core/data/src/test/kotlin/dev/mslalith/focuslauncher/core/data/repository/impl/settings/GeneralSettingsRepoTest.kt
+++ core/data/src/test/kotlin/dev/mslalith/focuslauncher/core/data/repository/impl/settings/GeneralSettingsRepoTest.kt
@@ -1,22 +1,40 @@
 package dev.mslalith.focuslauncher.core.data.repository.impl.settings
 
-import dev.mslalith.focuslauncher.core.data.base.RepoTest
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.HiltTestApplication
 import dev.mslalith.focuslauncher.core.data.helpers.verifyChange
-import dev.mslalith.focuslauncher.core.data.model.TestComponents
+import dev.mslalith.focuslauncher.core.data.repository.settings.GeneralSettingsRepo
 import dev.mslalith.focuslauncher.core.data.utils.Constants.Defaults.Settings.General.DEFAULT_FIRST_RUN
 import dev.mslalith.focuslauncher.core.data.utils.Constants.Defaults.Settings.General.DEFAULT_IS_DEFAULT_LAUNCHER
 import dev.mslalith.focuslauncher.core.data.utils.Constants.Defaults.Settings.General.DEFAULT_NOTIFICATION_SHADE
 import dev.mslalith.focuslauncher.core.data.utils.Constants.Defaults.Settings.General.DEFAULT_STATUS_BAR
+import dev.mslalith.focuslauncher.core.testing.CoroutineTest
+import javax.inject.Inject
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 @OptIn(ExperimentalCoroutinesApi::class)
+@HiltAndroidTest
 @RunWith(RobolectricTestRunner::class)
-internal class GeneralSettingsRepoImplTest : RepoTest<GeneralSettingsRepoImpl>() {
+@Config(application = HiltTestApplication::class)
+internal class GeneralSettingsRepoTest : CoroutineTest() {
 
-    override fun provideRepo(testComponents: TestComponents) = GeneralSettingsRepoImpl(settingsDataStore = testComponents.dataStore)
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    @Inject
+    lateinit var repo: GeneralSettingsRepo
+
+    @Before
+    fun setup() {
+        hiltRule.inject()
+    }
 
     @Test
     fun `verify first run change`() = runCoroutineTest {
