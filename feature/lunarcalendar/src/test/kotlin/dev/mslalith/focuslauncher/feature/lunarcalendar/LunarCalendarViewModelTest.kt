b795c217a0ce6c7efbbf1c0ae2bcfb2bdc7368b5
@msLalith <mslalith.opensource@gmail.com>
2023-04-01 10:47:50 +0530

    test: Add basic tests for LunarCalendarViewModel (#112)

---
+++ feature/lunarcalendar/src/test/kotlin/dev/mslalith/focuslauncher/feature/lunarcalendar/LunarCalendarViewModelTest.kt
@@ -0,0 +1,89 @@
+package dev.mslalith.focuslauncher.feature.lunarcalendar
+
+import com.google.common.truth.Truth.assertThat
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.HiltTestApplication
+import dev.mslalith.focuslauncher.core.common.appcoroutinedispatcher.AppCoroutineDispatcher
+import dev.mslalith.focuslauncher.core.data.repository.ClockRepo
+import dev.mslalith.focuslauncher.core.data.repository.LunarPhaseDetailsRepo
+import dev.mslalith.focuslauncher.core.data.repository.settings.LunarPhaseSettingsRepo
+import dev.mslalith.focuslauncher.core.testing.CoroutineTest
+import dev.mslalith.focuslauncher.core.testing.extensions.assertFor
+import dev.mslalith.focuslauncher.core.testing.extensions.awaitItem
+import javax.inject.Inject
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+@OptIn(ExperimentalCoroutinesApi::class)
+@HiltAndroidTest
+@RunWith(RobolectricTestRunner::class)
+@Config(application = HiltTestApplication::class)
+class LunarCalendarViewModelTest : CoroutineTest() {
+
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    @Inject
+    lateinit var clockRepo: ClockRepo
+
+    @Inject
+    lateinit var lunarPhaseDetailsRepo: LunarPhaseDetailsRepo
+
+    @Inject
+    lateinit var lunarPhSettingsRepo: LunarPhaseSettingsRepo
+
+    @Inject
+    lateinit var appCoroutineDispatcher: AppCoroutineDispatcher
+
+    private lateinit var viewModel: LunarCalendarViewModel
+
+    @Before
+    fun setup() {
+        hiltRule.inject()
+        viewModel = LunarCalendarViewModel(
+            clockRepo = clockRepo,
+            lunarPhaseDetailsRepo = lunarPhaseDetailsRepo,
+            lunarPhaseSettingsRepo = lunarPhSettingsRepo,
+            appCoroutineDispatcher = appCoroutineDispatcher
+        )
+    }
+
+    @Test
+    fun `on toggle lunar phase visibility, verify state change`() = runCoroutineTest {
+        assertThat(viewModel.lunarCalendarState.awaitItem().showLunarPhase).isTrue()
+
+        viewModel.toggleShowLunarPhase()
+        viewModel.lunarCalendarState.assertFor(expected = false) { it.showLunarPhase }
+
+        viewModel.toggleShowLunarPhase()
+        viewModel.lunarCalendarState.assertFor(expected = true) { it.showLunarPhase }
+    }
+
+    @Test
+    fun `on toggle lunar illumination visibility, verify state change`() = runCoroutineTest {
+        assertThat(viewModel.lunarCalendarState.awaitItem().showIlluminationPercent).isTrue()
+
+        viewModel.toggleShowIlluminationPercent()
+        viewModel.lunarCalendarState.assertFor(expected = false) { it.showIlluminationPercent }
+
+        viewModel.toggleShowIlluminationPercent()
+        viewModel.lunarCalendarState.assertFor(expected = true) { it.showIlluminationPercent }
+    }
+
+    @Test
+    fun `on toggle upcoming lunar visibility, verify state change`() = runCoroutineTest {
+        assertThat(viewModel.lunarCalendarState.awaitItem().showUpcomingPhaseDetails).isTrue()
+
+        viewModel.toggleShowUpcomingPhaseDetails()
+        viewModel.lunarCalendarState.assertFor(expected = false) { it.showUpcomingPhaseDetails }
+
+        viewModel.toggleShowUpcomingPhaseDetails()
+        viewModel.lunarCalendarState.assertFor(expected = true) { it.showUpcomingPhaseDetails }
+    }
+}
