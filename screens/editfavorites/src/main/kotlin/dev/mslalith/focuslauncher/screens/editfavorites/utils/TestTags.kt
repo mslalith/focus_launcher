03b0e9884b9e19eb460507f2fe06729a1461d74e
mslalith <mslalith.opensource@gmail.com>
2023-04-01 21:35:24 +0530

    chore: make TestTags internal

--- screens/editfavorites/src/main/kotlin/dev/mslalith/focuslauncher/screens/editfavorites/utils/TestTags.kt
+++ screens/editfavorites/src/main/kotlin/dev/mslalith/focuslauncher/screens/editfavorites/utils/TestTags.kt
@@ -1,6 +1,6 @@
 package dev.mslalith.focuslauncher.screens.editfavorites.utils
 
-object TestTags {
+internal object TestTags {
     const val TAG_FAVORITES_LIST_ITEM = "tag_favorites_list_item"
     const val TAG_FAVORITES_LIST = "tag_favorites_list"
     const val TAG_CLEAR_FAVORITES_FAB = "tag_clear_favorites_fab"
