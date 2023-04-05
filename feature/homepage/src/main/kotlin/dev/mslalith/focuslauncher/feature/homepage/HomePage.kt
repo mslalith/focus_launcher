f57482f8bb2619aff901ba7afd98eb8f5dcd520a
mslalith <mslalith.opensource@gmail.com>
2023-04-05 20:45:33 +0530

    chore: create stateless composable for HomePage

--- feature/homepage/src/main/kotlin/dev/mslalith/focuslauncher/feature/homepage/HomePage.kt
+++ feature/homepage/src/main/kotlin/dev/mslalith/focuslauncher/feature/homepage/HomePage.kt
@@ -11,17 +11,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.hilt.navigation.compose.hiltViewModel
 import dev.mslalith.focuslauncher.core.common.extensions.openNotificationShade
+import dev.mslalith.focuslauncher.core.model.App
 import dev.mslalith.focuslauncher.core.ui.extensions.onSwipeDown
 import dev.mslalith.focuslauncher.feature.clock24.ClockWidget
 import dev.mslalith.focuslauncher.feature.homepage.favorites.FavoritesList
+import dev.mslalith.focuslauncher.feature.homepage.model.FavoritesContextMode
 import dev.mslalith.focuslauncher.feature.homepage.model.HomePadding
+import dev.mslalith.focuslauncher.feature.homepage.model.HomePageState
 import dev.mslalith.focuslauncher.feature.homepage.model.LocalHomePadding
 import dev.mslalith.focuslauncher.feature.lunarcalendar.detailsdialog.LunarPhaseDetailsDialog
 import kotlinx.coroutines.flow.Flow
@@ -42,15 +44,57 @@ internal fun HomePage(
     homePageViewModel: HomePageViewModel,
     pagerCurrentPage: Flow<Int>
 ) {
-    val context = LocalContext.current
-    val homePageState by homePageViewModel.homePageState.collectAsState()
-    val showMoonCalendarDetailsDialog by homePageViewModel.showMoonCalendarDetailsDialogStateFlow.collectAsState()
-
     LaunchedEffect(key1 = pagerCurrentPage) {
         pagerCurrentPage.collectLatest { page ->
             if (page != 1) homePageViewModel.hideContextualMode()
         }
     }
+
+    MoonCalendarDetailsDialog(
+        showMoonCalendarDetailsDialogProvider = homePageViewModel.showMoonCalendarDetailsDialogStateFlow.collectAsState().value,
+        onHideMoonCalendarDetailsDialog = homePageViewModel::hideMoonCalendarDetailsDialog
+    )
+
+    HomePage(
+        homePageState = homePageViewModel.homePageState.collectAsState().value,
+        isInContextualMode = homePageViewModel::isInContextualMode,
+        hideContextualMode = homePageViewModel::hideContextualMode,
+        changeFavoritesContextMode = homePageViewModel::changeFavoritesContextMode,
+        onMoonCalendarClick = homePageViewModel::showMoonCalendarDetailsDialog,
+        addDefaultAppsIfRequired = homePageViewModel::addDefaultAppsIfRequired,
+        removeFromFavorites = homePageViewModel::removeFromFavorites,
+        reorderFavorite = homePageViewModel::reorderFavorite,
+        isAppAboutToReorder = homePageViewModel::isAppAboutToReorder,
+        isReordering = homePageViewModel::isReordering
+    )
+}
+
+@Composable
+internal fun MoonCalendarDetailsDialog(
+    showMoonCalendarDetailsDialogProvider: Boolean,
+    onHideMoonCalendarDetailsDialog: () -> Unit
+) {
+    if (showMoonCalendarDetailsDialogProvider) {
+        LunarPhaseDetailsDialog(
+            onClose = onHideMoonCalendarDetailsDialog
+        )
+    }
+}
+
+@Composable
+internal fun HomePage(
+    homePageState: HomePageState,
+    isInContextualMode: () -> Boolean,
+    hideContextualMode: () -> Unit,
+    changeFavoritesContextMode: (FavoritesContextMode) -> Unit,
+    onMoonCalendarClick: () -> Unit,
+    addDefaultAppsIfRequired: (List<App>) -> Unit,
+    removeFromFavorites: (App) -> Unit,
+    reorderFavorite: (App, App, () -> Unit) -> Unit,
+    isAppAboutToReorder: (App) -> Boolean,
+    isReordering: () -> Boolean,
+) {
+    val context = LocalContext.current
 
     CompositionLocalProvider(LocalHomePadding provides HomePadding()) {
         val contentPaddingValues = LocalHomePadding.current.contentPaddingValues
@@ -69,7 +113,7 @@ internal fun HomePage(
                 Spacer(modifier = Modifier.height(topPadding))
                 ClockWidget(horizontalPadding = horizontalPadding)
                 SpacedMoonCalendar(
-                    onMoonCalendarClick = homePageViewModel::showMoonCalendarDetailsDialog
+                    onMoonCalendarClick = onMoonCalendarClick
                 )
                 Box(modifier = Modifier.weight(1f)) {
                     DecoratedQuote(
@@ -78,25 +122,19 @@ internal fun HomePage(
                 }
                 FavoritesList(
                     favoritesList = homePageState.favoritesList,
-                    addDefaultAppsToFavorites = homePageViewModel::addDefaultAppsIfRequired,
-                    removeFromFavorites = homePageViewModel::removeFromFavorites,
-                    reorderFavorite = homePageViewModel::reorderFavorite,
+                    addDefaultAppsToFavorites = addDefaultAppsIfRequired,
+                    removeFromFavorites = removeFromFavorites,
+                    reorderFavorite = reorderFavorite,
                     currentContextMode1 = homePageState.favoritesContextualMode,
-                    isInContextualMode = homePageViewModel::isInContextualMode,
-                    isReordering = homePageViewModel::isReordering,
-                    hideContextualMode = homePageViewModel::hideContextualMode,
-                    changeFavoritesContextMode = homePageViewModel::changeFavoritesContextMode,
-                    isAppAboutToReorder = homePageViewModel::isAppAboutToReorder,
+                    isInContextualMode = isInContextualMode,
+                    isReordering = isReordering,
+                    hideContextualMode = hideContextualMode,
+                    changeFavoritesContextMode = changeFavoritesContextMode,
+                    isAppAboutToReorder = isAppAboutToReorder,
                     contentPadding = horizontalPadding
                 )
                 Spacer(modifier = Modifier.height(bottomPadding))
             }
         }
-    }
-
-    if (showMoonCalendarDetailsDialog) {
-        LunarPhaseDetailsDialog(
-            onClose = homePageViewModel::hideMoonCalendarDetailsDialog
-        )
     }
 }
