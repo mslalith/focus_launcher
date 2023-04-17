9bd1e6d809f12e76f1356bd81a6c97e0439af5b6
mslalith <mslalith.opensource@gmail.com>
2023-04-17 18:24:26 +0530

    chore: remove unused methods

--- core/common/src/main/kotlin/dev/mslalith/focuslauncher/core/common/extensions/ContextExtensions.kt
+++ core/common/src/main/kotlin/dev/mslalith/focuslauncher/core/common/extensions/ContextExtensions.kt
@@ -3,12 +3,8 @@ package dev.mslalith.focuslauncher.core.common.extensions
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
-import android.content.pm.ApplicationInfo
-import android.content.pm.PackageManager
 import android.net.Uri
 import android.provider.Settings
-import android.provider.Telephony
-import android.telecom.TelecomManager
 import android.widget.Toast
 import dev.mslalith.focuslauncher.core.model.App
 import java.lang.reflect.Method
@@ -24,43 +20,6 @@ fun Context.openNotificationShade() {
     val method: Method = statusBarManager.getMethod("expandNotificationsPanel")
     method.invoke(statusBarService)
 }
-
-fun Context.appNoIconModelOf(packageName: String): App? = try {
-    with(packageManager) {
-        val info = getApplicationInfo(packageName, 0)
-        val name = getApplicationLabel(info).toString()
-        val isSystem = isSystemApp(packageName)
-        App(
-            name = name,
-            displayName = name,
-            packageName = packageName,
-            isSystem = isSystem
-        )
-    }
-} catch (ex: PackageManager.NameNotFoundException) {
-    null
-}
-
-fun Context.isSystemApp(packageName: String) = try {
-    (
-        packageManager.getApplicationInfo(packageName, 0).flags and
-            (ApplicationInfo.FLAG_SYSTEM or ApplicationInfo.FLAG_UPDATED_SYSTEM_APP)
-        ) != 0
-} catch (ex: PackageManager.NameNotFoundException) {
-    false
-}
-
-val Context.defaultDialerApp: App?
-    get() {
-        val manager = getSystemService(Context.TELECOM_SERVICE) as TelecomManager
-        return manager.defaultDialerPackage?.let { appNoIconModelOf(it) }
-    }
-
-val Context.defaultMessagingApp: App?
-    get() {
-        val packageName: String? = Telephony.Sms.getDefaultSmsPackage(this)
-        return packageName?.let { appNoIconModelOf(packageName) }
-    }
 
 fun Context.launchApp(app: App) {
     packageManager.getLaunchIntentForPackage(app.packageName)?.let {
