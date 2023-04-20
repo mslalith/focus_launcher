f62ff52af1fe76702d2edb0e507812d237fa8c2a
mslalith <mslalith.opensource@gmail.com>
2023-04-20 13:36:10 +0530

    chore: upgrade to Java 17

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 group = "dev.mslalith.focuslauncher.buildlogic"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 tasks.withType<KotlinCompile>().configureEach {
-    kotlinOptions.jvmTarget = JavaVersion.VERSION_11.toString()
+    kotlinOptions.jvmTarget = JavaVersion.VERSION_17.toString()
 }
 
 dependencies {
