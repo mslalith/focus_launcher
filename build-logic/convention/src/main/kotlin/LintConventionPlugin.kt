8a76a5d615fce77b8d1797e315f3ccb40dc02839
mslalith <mslalith.opensource@gmail.com>
2023-04-23 09:07:52 +0530

    chore: add detekt-formatting

--- build-logic/convention/src/main/kotlin/LintConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/LintConventionPlugin.kt
@@ -1,12 +1,18 @@
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
 import org.gradle.kotlin.dsl.project
 
 class LintConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) = with(target) {
+        pluginManager.apply("io.gitlab.arturbosch.detekt")
+
+        val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
         dependencies {
             add("implementation", project(":core:lint"))
+            add("detektPlugins", libs.findLibrary("detekt").get())
         }
     }
 }
