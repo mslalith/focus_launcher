ba6a69b96cd4c270510d117c45c22abd53f109fb
mslalith <mslalith.opensource@gmail.com>
2023-04-30 18:14:36 +0530

    chore: handle deprecations

--- core/ui/src/main/kotlin/dev/mslalith/focuslauncher/core/ui/SelectableIconItem.kt
+++ core/ui/src/main/kotlin/dev/mslalith/focuslauncher/core/ui/SelectableIconItem.kt
@@ -2,7 +2,6 @@ package dev.mslalith.focuslauncher.core.ui
 
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.clickable
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.ListItemDefaults
@@ -13,7 +12,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SelectableIconItem(
     modifier: Modifier = Modifier,
