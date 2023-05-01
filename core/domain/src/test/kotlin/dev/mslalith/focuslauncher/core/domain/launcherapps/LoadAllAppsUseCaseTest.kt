9ec15c9eed8d956259d9e87eb213dfa87b2819da
mslalith <mslalith.opensource@gmail.com>
2023-05-01 10:17:27 +0530

    chore: add more tests to LoadAllAppsUseCaseTest

--- core/domain/src/test/kotlin/dev/mslalith/focuslauncher/core/domain/launcherapps/LoadAllAppsUseCaseTest.kt
+++ core/domain/src/test/kotlin/dev/mslalith/focuslauncher/core/domain/launcherapps/LoadAllAppsUseCaseTest.kt
@@ -1,6 +1,5 @@
 package dev.mslalith.focuslauncher.core.domain.launcherapps
 
-import app.cash.turbine.test
 import com.google.common.truth.Truth.assertThat
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
@@ -9,12 +8,16 @@ import dev.mslalith.focuslauncher.core.data.repository.AppDrawerRepo
 import dev.mslalith.focuslauncher.core.launcherapps.manager.launcherapps.test.TestLauncherAppsManager
 import dev.mslalith.focuslauncher.core.testing.CoroutineTest
 import dev.mslalith.focuslauncher.core.testing.TestApps
+import dev.mslalith.focuslauncher.core.testing.extensions.awaitItem
+import io.mockk.spyk
+import io.mockk.verify
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.launch
 import org.junit.Before
+import org.junit.FixMethodOrder
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.junit.runners.MethodSorters
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 import javax.inject.Inject
@@ -23,6 +26,7 @@ import javax.inject.Inject
 @HiltAndroidTest
 @RunWith(RobolectricTestRunner::class)
 @Config(application = HiltTestApplication::class)
+@FixMethodOrder(value = MethodSorters.NAME_ASCENDING)
 class LoadAllAppsUseCaseTest : CoroutineTest() {
 
     @get:Rule(order = 0)
@@ -31,26 +35,64 @@ class LoadAllAppsUseCaseTest : CoroutineTest() {
     @Inject
     lateinit var appDrawerRepo: AppDrawerRepo
 
+    private val launcherAppsManager = spyk<TestLauncherAppsManager>()
+
     private lateinit var useCase: LoadAllAppsUseCase
 
     @Before
     fun setup() {
         hiltRule.inject()
         useCase = LoadAllAppsUseCase(
-            launcherAppsManager = TestLauncherAppsManager(),
+            launcherAppsManager = launcherAppsManager,
             appDrawerRepo = appDrawerRepo
         )
     }
 
     @Test
-    fun `when installed apps are queried and added, they must be added to apps DB`() = runCoroutineTest {
-        backgroundScope.launch {
-            appDrawerRepo.allAppsFlow.test {
-                assertThat(awaitItem()).isEmpty()
-                assertThat(awaitItem()).isEqualTo(TestApps.all)
-            }
-        }
+    fun `1 - when installed apps are queried and added, they must be added to apps DB`() = runCoroutineTest {
+        assertThat(appDrawerRepo.allAppsFlow.awaitItem()).isEmpty()
+
+        launcherAppsManager.setAllApps(apps = TestApps.all)
+        useCase()
+
+        assertThat(appDrawerRepo.allAppsFlow.awaitItem()).isEqualTo(TestApps.all)
+    }
+
+    @Test
+    fun `2 - when force loading apps, new set of apps must be added to apps DB`() = runCoroutineTest {
+        assertThat(appDrawerRepo.allAppsFlow.awaitItem()).isEmpty()
+        var appsAdded = listOf(TestApps.Chrome)
+
+        launcherAppsManager.setAllApps(apps = appsAdded)
+        useCase()
+        assertThat(appDrawerRepo.allAppsFlow.awaitItem()).isEqualTo(appsAdded)
+
+        appsAdded = TestApps.all
+
+        launcherAppsManager.setAllApps(apps = appsAdded)
+        useCase(forceLoad = true)
+        assertThat(appDrawerRepo.allAppsFlow.awaitItem()).isEqualTo(appsAdded)
+    }
+
+    @Test
+    fun `3 - when installed apps are loaded, they must not be loaded again`() = runCoroutineTest {
+        launcherAppsManager.setAllApps(apps = TestApps.all)
+        useCase()
+
+        assertThat(appDrawerRepo.allAppsFlow.awaitItem()).isEqualTo(TestApps.all)
 
         useCase()
+        verify(exactly = 1) { launcherAppsManager.loadAllApps() }
+    }
+
+    @Test
+    fun `4 - when installed apps are queried and added, they must be added to apps DB`() = runCoroutineTest {
+        launcherAppsManager.setAllApps(apps = TestApps.all)
+        useCase()
+
+        assertThat(appDrawerRepo.allAppsFlow.awaitItem()).isEqualTo(TestApps.all)
+
+        useCase(forceLoad = true)
+        verify(exactly = 2) { launcherAppsManager.loadAllApps() }
     }
 }
--- core/testing/src/main/kotlin/dev/mslalith/focuslauncher/core/testing/rules/TestCoroutineRule.kt
+++ core/testing/src/main/kotlin/dev/mslalith/focuslauncher/core/testing/rules/TestCoroutineRule.kt
@@ -1,19 +1,19 @@
 package dev.mslalith.focuslauncher.core.testing.rules
 
-import kotlin.coroutines.CoroutineContext
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.TestDispatcher
 import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
+import kotlin.coroutines.CoroutineContext
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class TestCoroutineRule(
-    val testDispatcher: TestDispatcher = StandardTestDispatcher()
+    val testDispatcher: TestDispatcher = UnconfinedTestDispatcher()
 ) : TestWatcher() {
 
     override fun starting(description: Description) {
