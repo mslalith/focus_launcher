30adb94cd6c091322920e77e40eb87e0e4488d7a
mslalith <mslalith.opensource@gmail.com>
2023-05-01 09:20:56 +0530

    chore: add LoadAllAppsUseCase

---
+++ core/domain/src/main/kotlin/dev/mslalith/focuslauncher/core/domain/launcherapps/LoadAllAppsUseCase.kt
@@ -0,0 +1,17 @@
+package dev.mslalith.focuslauncher.core.domain.launcherapps
+
+import dev.mslalith.focuslauncher.core.data.repository.AppDrawerRepo
+import dev.mslalith.focuslauncher.core.launcherapps.manager.launcherapps.LauncherAppsManager
+import javax.inject.Inject
+
+class LoadAllAppsUseCase @Inject constructor(
+    private val launcherAppsManager: LauncherAppsManager,
+    private val appDrawerRepo: AppDrawerRepo
+) {
+    suspend operator fun invoke(forceLoad: Boolean = false) {
+        appDrawerRepo.apply {
+            if (!forceLoad && !areAppsEmptyInDatabase()) return
+            addApps(apps = launcherAppsManager.loadAllApps())
+        }
+    }
+}
