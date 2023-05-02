a5cba92a7dfaeb62097d2e79d9234997bbedc3e6
mslalith <mslalith.opensource@gmail.com>
2023-05-02 09:15:44 +0530

    chore: move Theme to res

--- core/model/src/main/kotlin/dev/mslalith/focuslauncher/core/model/Theme.kt
+++ core/model/src/main/kotlin/dev/mslalith/focuslauncher/core/model/Theme.kt
@@ -1,7 +1,7 @@
 package dev.mslalith.focuslauncher.core.model
 
-enum class Theme(val text: String) {
-    FOLLOW_SYSTEM(text = "Follow System"),
-    NOT_WHITE(text = "Not White"),
-    SAID_DARK(text = "Said Dark")
+enum class Theme(val uiText: UiText) {
+    FOLLOW_SYSTEM(uiText = UiText.Resource(stringRes = R.string.follow_system)),
+    NOT_WHITE(uiText = UiText.Resource(stringRes = R.string.not_white)),
+    SAID_DARK(uiText = UiText.Resource(stringRes = R.string.said_dark))
 }
