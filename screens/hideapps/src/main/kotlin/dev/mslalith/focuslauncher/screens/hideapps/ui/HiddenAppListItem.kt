164dadf29e74c7dbd665e2f0bf5a6a398ff83ce1
mslalith <mslalith.opensource@gmail.com>
2023-05-02 22:30:51 +0530

    fix: confirm background color is not transparent

--- screens/hideapps/src/main/kotlin/dev/mslalith/focuslauncher/screens/hideapps/ui/HiddenAppListItem.kt
+++ screens/hideapps/src/main/kotlin/dev/mslalith/focuslauncher/screens/hideapps/ui/HiddenAppListItem.kt
@@ -1,6 +1,5 @@
 package dev.mslalith.focuslauncher.screens.hideapps.ui
 
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -29,7 +28,6 @@ internal fun HiddenAppListItem(
             itemType = ConfirmSelectableItemType.Checkbox(
                 checked = selectedHiddenApp.isSelected
             ),
-            backgroundColor = MaterialTheme.colorScheme.surface,
             confirmUiText = UiText.Resource(stringRes = R.string.yes_comma_hide),
             onConfirm = onAppClick
         )
