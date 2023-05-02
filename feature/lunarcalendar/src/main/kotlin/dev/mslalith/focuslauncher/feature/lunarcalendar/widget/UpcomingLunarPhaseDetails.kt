25a5e363a12ac69dcd30c67d14883ae4109ea0f4
mslalith <mslalith.opensource@gmail.com>
2023-05-02 10:12:40 +0530

    chore: move LunarPhase to res

--- feature/lunarcalendar/src/main/kotlin/dev/mslalith/focuslauncher/feature/lunarcalendar/widget/UpcomingLunarPhaseDetails.kt
+++ feature/lunarcalendar/src/main/kotlin/dev/mslalith/focuslauncher/feature/lunarcalendar/widget/UpcomingLunarPhaseDetails.kt
@@ -5,8 +5,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import dev.mslalith.focuslauncher.core.common.extensions.inShortReadableFormat
 import dev.mslalith.focuslauncher.core.model.lunarphase.UpcomingLunarPhase
+import dev.mslalith.focuslauncher.core.ui.extensions.string
+import dev.mslalith.focuslauncher.feature.lunarcalendar.R
 
 @Composable
 internal fun UpcomingLunarPhaseDetails(
@@ -14,9 +17,9 @@ internal fun UpcomingLunarPhaseDetails(
     upcomingLunarPhase: UpcomingLunarPhase,
     textColor: Color
 ) {
-    val phaseName = upcomingLunarPhase.lunarPhase.phaseName
+    val phaseName = upcomingLunarPhase.lunarPhase.phaseNameUiText.string()
     val dateTime = upcomingLunarPhase.dateTime?.inShortReadableFormat() ?: return
-    val nextPhaseOnText = "next $phaseName is on $dateTime"
+    val nextPhaseOnText = stringResource(id = R.string.next_phase_is_on_date, phaseName, dateTime)
 
     Crossfade(
         modifier = modifier,
