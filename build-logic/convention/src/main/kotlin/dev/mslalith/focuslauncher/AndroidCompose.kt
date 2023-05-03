455177ff5acf439276bbe43f649ce225d77598e6
mslalith <mslalith.opensource@gmail.com>
2023-05-03 08:20:45 +0530

    chore: add Compose Metrics

--- build-logic/convention/src/main/kotlin/dev/mslalith/focuslauncher/AndroidCompose.kt
+++ build-logic/convention/src/main/kotlin/dev/mslalith/focuslauncher/AndroidCompose.kt
@@ -5,6 +5,9 @@ import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.getByType
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.io.File
 
 internal fun Project.configureAndroidCompose(
     commonExtension: CommonExtension<*, *, *, *>,
@@ -19,6 +22,12 @@ internal fun Project.configureAndroidCompose(
         kotlinCompilerExtensionVersion = libs.findVersion("androidxComposeCompiler").get().toString()
     }
 
+    tasks.withType<KotlinCompile>().configureEach {
+        kotlinOptions {
+            freeCompilerArgs += buildComposeMetricsParameters()
+        }
+    }
+
     dependencies {
         val bom = libs.findLibrary("androidx.compose.bom").get()
         add(configurationName = "implementation", platform(bom))
@@ -28,4 +37,28 @@ internal fun Project.configureAndroidCompose(
         add(configurationName = "implementation", libs.findLibrary("androidx.compose.ui").get())
         add(configurationName = "implementation", libs.findLibrary("androidx.compose.ui.tooling").get())
     }
+}
+
+private fun Project.buildComposeMetricsParameters(): List<String> {
+    val metricParameters = mutableListOf<String>()
+    val enableMetricsProvider = project.providers.gradleProperty("enableComposeCompilerMetrics")
+    val enableMetrics = (enableMetricsProvider.orNull == "true")
+    if (enableMetrics) {
+        val metricsFolder = File(project.buildDir, "compose-metrics")
+        metricParameters.add("-P")
+        metricParameters.add(
+            "plugin:androidx.compose.compiler.plugins.kotlin:metricsDestination=" + metricsFolder.absolutePath
+        )
+    }
+
+    val enableReportsProvider = project.providers.gradleProperty("enableComposeCompilerReports")
+    val enableReports = (enableReportsProvider.orNull == "true")
+    if (enableReports) {
+        val reportsFolder = File(project.buildDir, "compose-reports")
+        metricParameters.add("-P")
+        metricParameters.add(
+            "plugin:androidx.compose.compiler.plugins.kotlin:reportsDestination=" + reportsFolder.absolutePath
+        )
+    }
+    return metricParameters.toList()
 }
