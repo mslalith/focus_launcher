cf46318c6e37168054c524a351325d6f14cca1c9
mslalith <mslalith.opensource@gmail.com>
2023-05-21 11:21:50 +0530

    chore: remove logs

--- core/testing/src/main/kotlin/dev/mslalith/focuslauncher/core/testing/extensions/TurbineAssertExtensions.kt
+++ core/testing/src/main/kotlin/dev/mslalith/focuslauncher/core/testing/extensions/TurbineAssertExtensions.kt
@@ -8,19 +8,15 @@ import kotlinx.coroutines.flow.Flow
 context (CoroutineScope)
 suspend fun <T, R> Flow<T>.assertFor(
     expected: R,
-    debug: Boolean = false,
     valueFor: (T) -> R
 ) {
     val turbine = testIn(scope = this@CoroutineScope)
     var changedItem = valueFor(turbine.expectMostRecentItem())
-    if (debug) println("initial item: $changedItem")
 
     if (changedItem != expected) {
         changedItem = awaitItemChange(valueFor)
-        if (debug) println("changed item: $changedItem")
     }
 
     turbine.cancelAndIgnoreRemainingEvents()
-    if (debug) println("comparing: $changedItem to $expected")
     assertThat(changedItem).isEqualTo(expected)
 }
