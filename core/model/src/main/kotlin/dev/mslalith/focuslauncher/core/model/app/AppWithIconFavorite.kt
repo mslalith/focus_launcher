565f464529b6751e2267e7284b8122f73aab3448
mslalith <mslalith.opensource@gmail.com>
2023-05-28 09:03:12 +0530

    chore: remove AppWithIconFavorite

--- core/model/src/main/kotlin/dev/mslalith/focuslauncher/core/model/app/AppWithIconFavorite.kt
+++
@@ -1,9 +0,0 @@
-package dev.mslalith.focuslauncher.core.model.app
-
-import androidx.compose.runtime.Immutable
-
-@Immutable
-data class AppWithIconFavorite(
-    val appWithIcon: AppWithIcon,
-    val isFavorite: Boolean
-)
