a1563838aebd72af903879741053d7f8f31e53db
mslalith <mslalith.opensource@gmail.com>
2023-06-03 11:42:10 +0530

    chore: add tests IconPackViewModel

--- core/testing/build.gradle.kts
+++ core/testing/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
 dependencies {
     implementation(project(":core:model"))
-    implementation(project(":core:launcherapps"))
+    api(project(":core:launcherapps"))
 
     api(libs.junit4)
     implementation(libs.androidx.test.junit)
