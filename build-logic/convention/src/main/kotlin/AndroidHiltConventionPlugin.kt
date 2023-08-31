d4a0a6c56394b9ff38919bef1d7d2665656ddebb
mslalith <mslalith.opensource@gmail.com>
2023-08-31 21:30:09 +0530

    chore: migrate Hilt to KSP

--- build-logic/convention/src/main/kotlin/AndroidHiltConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidHiltConventionPlugin.kt
@@ -8,18 +8,16 @@ class AndroidHiltConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) = with(target) {
         with(pluginManager) {
             apply("dagger.hilt.android.plugin")
-            // KAPT must go last to avoid build warnings.
-            // See: https://stackoverflow.com/questions/70550883/warning-the-following-options-were-not-recognized-by-any-processor-dagger-f
-            apply("org.jetbrains.kotlin.kapt")
+            apply("com.google.devtools.ksp")
         }
 
         val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
         dependencies {
             add(configurationName = "implementation", libs.findLibrary("hilt.android").get())
-            add(configurationName = "kapt", libs.findLibrary("hilt.compiler").get())
-            add(configurationName = "kapt", libs.findLibrary("hilt.android.compiler").get())
-            add(configurationName = "kaptTest", libs.findLibrary("hilt.android.compiler").get())
-            add(configurationName = "kaptAndroidTest", libs.findLibrary("hilt.android.compiler").get())
+            add(configurationName = "ksp", libs.findLibrary("hilt.compiler").get())
+            add(configurationName = "ksp", libs.findLibrary("hilt.android.compiler").get())
+            add(configurationName = "kspTest", libs.findLibrary("hilt.android.compiler").get())
+            add(configurationName = "kspAndroidTest", libs.findLibrary("hilt.android.compiler").get())
             add(configurationName = "implementation", libs.findLibrary("hilt.android.testing").get())
         }
     }
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.kotlin.serialization) apply false
-    alias(libs.plugins.kotlin.kapt) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.kotlinx.kover) apply false
     alias(libs.plugins.detekt)
