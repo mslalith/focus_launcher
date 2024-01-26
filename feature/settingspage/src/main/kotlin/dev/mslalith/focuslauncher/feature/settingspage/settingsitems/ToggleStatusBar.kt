932345a75ea7dc67532015f2f4da08eb843af444
mslalith <mslalith.opensource@gmail.com>
2024-01-26 10:47:24 +0530

    fix: status bar is flickering upon coming back from any screen

--- feature/settingspage/src/main/kotlin/dev/mslalith/focuslauncher/feature/settingspage/settingsitems/ToggleStatusBar.kt
+++ feature/settingspage/src/main/kotlin/dev/mslalith/focuslauncher/feature/settingspage/settingsitems/ToggleStatusBar.kt
@@ -2,6 +2,7 @@ package dev.mslalith.focuslauncher.feature.settingspage.settingsitems
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import dev.mslalith.focuslauncher.core.testing.compose.modifier.testsemantics.testSemantics
@@ -10,7 +11,11 @@ import dev.mslalith.focuslauncher.core.ui.providers.LocalSystemUiController
 import dev.mslalith.focuslauncher.feature.settingspage.R
 import dev.mslalith.focuslauncher.feature.settingspage.shared.SettingsItem
 import dev.mslalith.focuslauncher.feature.settingspage.utils.TestTags
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.debounce
 
+@OptIn(FlowPreview::class)
 @Composable
 internal fun ToggleStatusBar(
     showStatusBar: Boolean,
@@ -19,7 +24,13 @@ internal fun ToggleStatusBar(
     val systemUiController = LocalSystemUiController.current
 
     LaunchedEffect(key1 = showStatusBar) {
-        systemUiController.toggleStatusBars(show = showStatusBar)
+        // default value will be emitted for showStatusBar
+        // debounce to avoid a flicker when coming back from any screen
+        snapshotFlow { showStatusBar }
+            .debounce(timeoutMillis = 200)
+            .collectLatest {
+                systemUiController.toggleStatusBars(show = showStatusBar)
+            }
     }
 
     val stringRes = when (showStatusBar) {
