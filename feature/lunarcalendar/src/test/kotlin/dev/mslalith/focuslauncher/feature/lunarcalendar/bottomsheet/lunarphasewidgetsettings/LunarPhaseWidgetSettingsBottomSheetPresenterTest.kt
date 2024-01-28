aeaef874fdac9500e23f3d5e8cc59368aab96e89
mslalith <mslalith.opensource@gmail.com>
2024-01-28 10:09:35 +0530

    chore: add LunarPhaseWidgetSettingsBottomSheetPresenterTest

---
+++ feature/lunarcalendar/src/test/kotlin/dev/mslalith/focuslauncher/feature/lunarcalendar/bottomsheet/lunarphasewidgetsettings/LunarPhaseWidgetSettingsBottomSheetPresenterTest.kt
@@ -0,0 +1,71 @@
+package dev.mslalith.focuslauncher.feature.lunarcalendar.bottomsheet.lunarphasewidgetsettings
+
+import com.google.common.truth.Truth.assertThat
+import dev.mslalith.focuslauncher.core.circuitoverlay.OverlayResultScreen
+import dev.mslalith.focuslauncher.core.common.appcoroutinedispatcher.test.TestAppCoroutineDispatcher
+import dev.mslalith.focuslauncher.core.data.test.repository.FakeClockRepo
+import dev.mslalith.focuslauncher.core.data.test.repository.FakeLunarPhaseDetailsRepo
+import dev.mslalith.focuslauncher.core.data.test.repository.settings.FakeLunarPhaseSettingsRepo
+import dev.mslalith.focuslauncher.core.screens.CurrentPlaceScreen
+import dev.mslalith.focuslauncher.core.screens.LunarPhaseWidgetSettingsBottomSheetScreen
+import dev.mslalith.focuslauncher.core.testing.AppRobolectricTestRunner
+import dev.mslalith.focuslauncher.core.testing.circuit.PresenterTest
+import org.junit.FixMethodOrder
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.MethodSorters
+
+@RunWith(AppRobolectricTestRunner::class)
+@FixMethodOrder(value = MethodSorters.NAME_ASCENDING)
+class LunarPhaseWidgetSettingsBottomSheetPresenterTest : PresenterTest<LunarPhaseWidgetSettingsBottomSheetPresenter, LunarPhaseWidgetSettingsBottomSheetState>() {
+
+    private val clockRepo = FakeClockRepo()
+    private val lunarPhaseSettingsRepo = FakeLunarPhaseSettingsRepo()
+    private val lunarPhaseDetailsRepo = FakeLunarPhaseDetailsRepo()
+    private val appCoroutineDispatcher = TestAppCoroutineDispatcher()
+
+    override fun presenterUnderTest() = LunarPhaseWidgetSettingsBottomSheetPresenter(
+        navigator = navigator,
+        clockRepo = clockRepo,
+        lunarPhaseSettingsRepo = lunarPhaseSettingsRepo,
+        lunarPhaseDetailsRepo = lunarPhaseDetailsRepo,
+        appCoroutineDispatcher = appCoroutineDispatcher
+    )
+
+    @Test
+    fun `01 - when show lunar phase toggled, verify state change`() = runPresenterTest {
+        val state = awaitItem()
+        assertThat(state.showLunarPhase).isTrue()
+
+        state.eventSink(LunarPhaseWidgetSettingsBottomSheetUiEvent.ToggleShowLunarPhase)
+        assertForFalse { it.showLunarPhase }
+    }
+
+    @Test
+    fun `02 - when show illumination percent toggled, verify state change`() = runPresenterTest {
+        val state = awaitItem()
+        assertThat(state.showIlluminationPercent).isTrue()
+
+        state.eventSink(LunarPhaseWidgetSettingsBottomSheetUiEvent.ToggleShowIlluminationPercent)
+        assertForFalse { it.showIlluminationPercent }
+    }
+
+    @Test
+    fun `03 - when show upcoming phase details toggled, verify state change`() = runPresenterTest {
+        val state = awaitItem()
+        assertThat(state.showUpcomingPhaseDetails).isTrue()
+
+        state.eventSink(LunarPhaseWidgetSettingsBottomSheetUiEvent.ToggleShowUpcomingPhaseDetails)
+        assertForFalse { it.showUpcomingPhaseDetails }
+    }
+
+    @Test
+    fun `04 - verify goTo screen`() = runPresenterTest {
+        val state = awaitItem()
+        state.eventSink(LunarPhaseWidgetSettingsBottomSheetUiEvent.Goto(CurrentPlaceScreen))
+
+        @Suppress("UNCHECKED_CAST")
+        val nextScreen = navigator.awaitNextScreen() as OverlayResultScreen<LunarPhaseWidgetSettingsBottomSheetScreen.Result.PopAndGoto>
+        assertThat(nextScreen.result?.screen).isEqualTo(CurrentPlaceScreen)
+    }
+}
