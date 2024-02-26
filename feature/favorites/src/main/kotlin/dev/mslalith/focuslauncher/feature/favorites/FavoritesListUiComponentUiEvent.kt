629a1af6c1507ef454e7f9aaa64f229cb2550bce
mslalith <mslalith.opensource@gmail.com>
2024-02-26 14:22:01 +0530

    chore: remove old way of re-ordering and deleting favorites

--- feature/favorites/src/main/kotlin/dev/mslalith/focuslauncher/feature/favorites/FavoritesListUiComponentUiEvent.kt
+++ feature/favorites/src/main/kotlin/dev/mslalith/focuslauncher/feature/favorites/FavoritesListUiComponentUiEvent.kt
@@ -1,12 +1,7 @@
 package dev.mslalith.focuslauncher.feature.favorites
 
 import com.slack.circuit.runtime.CircuitUiEvent
-import dev.mslalith.focuslauncher.core.model.app.App
-import dev.mslalith.focuslauncher.feature.favorites.model.FavoritesContextMode
 
 sealed interface FavoritesListUiComponentUiEvent : CircuitUiEvent {
     data object AddDefaultAppsIfRequired : FavoritesListUiComponentUiEvent
-    data class RemoveFromFavorites(val app: App) : FavoritesListUiComponentUiEvent
-    data class ReorderFavorite(val app: App, val withApp: App, val onReordered: () -> Unit) : FavoritesListUiComponentUiEvent
-    data class UpdateFavoritesContextMode(val mode: FavoritesContextMode) : FavoritesListUiComponentUiEvent
 }
--- feature/favorites/src/main/kotlin/dev/mslalith/focuslauncher/feature/favorites/ui/FavoriteItem.kt
+++ feature/favorites/src/main/kotlin/dev/mslalith/focuslauncher/feature/favorites/ui/FavoriteItem.kt
@@ -1,7 +1,5 @@
 package dev.mslalith.focuslauncher.feature.favorites.ui
 
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -12,64 +10,43 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import dev.mslalith.focuslauncher.core.model.app.AppWithColor
 import dev.mslalith.focuslauncher.core.ui.remember.rememberAppColor
-import dev.mslalith.focuslauncher.feature.favorites.model.FavoritesContextMode
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 internal fun FavoriteItem(
-    modifier: Modifier = Modifier,
     appWithColor: AppWithColor,
-    isInContextualMode: () -> Boolean,
-    isAppAboutToReorder: () -> Boolean,
-    changeFavoritesContextMode: (FavoritesContextMode) -> Unit,
-    onClick: () -> Unit
+    onClick: () -> Unit,
+    onLongClick: () -> Unit,
+    modifier: Modifier = Modifier
 ) {
-    val backgroundColor = MaterialTheme.colorScheme.surface
-    val contentColor = MaterialTheme.colorScheme.onSurface
-
     val appIconBasedColor = rememberAppColor(graphicsColor = appWithColor.color)
-
-    val animatedColor by animateColorAsState(
-        label = "Animated color",
-        targetValue = if (isAppAboutToReorder()) contentColor else appIconBasedColor,
-        animationSpec = tween(durationMillis = 2300)
-    )
-
-    fun backgroundColor(): Color = animatedColor.copy(alpha = if (isAppAboutToReorder()) 0.8f else 0.23f)
-
-    fun textColor(): Color = if (isAppAboutToReorder()) backgroundColor else animatedColor
 
     Row(
         modifier = modifier
             .clip(shape = MaterialTheme.shapes.small)
-            .background(color = backgroundColor())
+            .background(color = appIconBasedColor.copy(alpha = 0.23f))
             .border(
                 width = 0.4.dp,
-                color = animatedColor.copy(alpha = 0.6f),
+                color = appIconBasedColor.copy(alpha = 0.6f),
                 shape = MaterialTheme.shapes.small
             )
             .combinedClickable(
                 onClick = onClick,
-                onLongClick = {
-                    if (isInContextualMode()) return@combinedClickable
-                    changeFavoritesContextMode(FavoritesContextMode.Open)
-                }
+                onLongClick = onLongClick
             )
             .padding(horizontal = 16.dp, vertical = 8.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
             text = appWithColor.app.displayName,
-            color = textColor(),
+            color = appIconBasedColor,
             style = MaterialTheme.typography.bodyMedium,
             textAlign = TextAlign.Center,
             modifier = Modifier.fillMaxWidth()
