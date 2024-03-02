c8a286f4802099a838a62d552783943f1ed31886
mslalith <mslalith.opensource@gmail.com>
2024-03-02 20:20:19 +0530

    chore: prepare for v0.9.0 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
 
     defaultConfig {
         applicationId = "dev.mslalith.focuslauncher"
-        versionCode = 13
-        versionName = "0.8.1"
+        versionCode = 15
+        versionName = "0.9.0"
         setProperty("archivesBaseName", "Focus-Launcher-v$versionName")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
