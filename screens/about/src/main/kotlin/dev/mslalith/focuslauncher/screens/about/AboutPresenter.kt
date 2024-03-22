28dd9887f71e443772d760e3e5d10e6ade1d59c8
mslalith <mslalith.opensource@gmail.com>
2024-03-22 09:59:50 +0530

    fix: assisted injection in AboutPresenter

--- screens/about/src/main/kotlin/dev/mslalith/focuslauncher/screens/about/AboutPresenter.kt
+++ screens/about/src/main/kotlin/dev/mslalith/focuslauncher/screens/about/AboutPresenter.kt
@@ -5,14 +5,20 @@ import com.slack.circuit.codegen.annotations.CircuitInject
 import com.slack.circuit.runtime.Navigator
 import com.slack.circuit.runtime.presenter.Presenter
 import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 import dagger.hilt.components.SingletonComponent
 import dev.mslalith.focuslauncher.core.screens.AboutScreen
 
-@CircuitInject(AboutScreen::class, SingletonComponent::class)
 class AboutPresenter @AssistedInject constructor(
     @Assisted private val navigator: Navigator
 ) : Presenter<AboutState> {
+
+    @CircuitInject(AboutScreen::class, SingletonComponent::class)
+    @AssistedFactory
+    fun interface Factory {
+        fun create(navigator: Navigator): AboutPresenter
+    }
 
     @Composable
     override fun present(): AboutState {
